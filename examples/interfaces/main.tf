6e23fbe163ac72ddd07193d5d5fb79e34dae7695
donovm4 <mccoy.donov@gmail.com>
2024-01-30 09:57:46 -0500

    updating versions specified in examples' main.tf

--- examples/interfaces/main.tf
+++ examples/interfaces/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.5.2"
+  required_version = "~> 1.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0"
+      version = "~> 3.7"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.0"
+      version = "~> 3.5"
     }
   }
 }
--- examples/public/main.tf
+++ examples/public/main.tf
@@ -2,15 +2,15 @@
 # false positive with public ip address and private ip address version
 
 terraform {
-  required_version = ">= 1.5.2"
+  required_version = "~> 1.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0"
+      version = "~> 3.7"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.0"
+      version = "~> 3.5"
     }
   }
 }
