2f1c1536d26c3e89c52e1115e91078428af5ae97
donovm4 <mccoy.donov@gmail.com>
2024-02-23 10:19:17 -0500

    fix versioning for 0.1.3 to 0.1.4

--- examples/gateway/main.tf
+++ examples/gateway/main.tf
@@ -216,7 +216,7 @@ module "standard_loadbalancer" {
   source = "../.."
 
   # source = "Azure/avm-res-network-loadbalancer/azurerm"
-  # version = 0.1.3
+  # version = 0.1.4
 
   enable_telemetry = var.enable_telemetry
 
--- examples/interfaces/README.md
+++ examples/interfaces/README.md
@@ -76,7 +76,7 @@ module "loadbalancer" {
   source = "../../"
 
   # source = "Azure/avm-res-network-loadbalancer/azurerm"
-  # version = 0.1.3
+  # version = 0.1.4
 
   enable_telemetry = var.enable_telemetry
 
