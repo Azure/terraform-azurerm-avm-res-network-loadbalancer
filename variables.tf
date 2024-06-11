3e7967a7852669b8fc6b64cfdb04769b65f55724
donovm4 <mccoy.donov@gmail.com>
2024-06-11 15:54:02 -0400

    linting fix sprint 2 for role_assignments

--- variables.tf
+++ variables.tf
@@ -819,6 +819,7 @@ variable "role_assignments" {
     condition                              = optional(string, null)
     condition_version                      = optional(string, null) # Valid values are 2.0
     delegated_managed_identity_resource_id = optional(string, null)
+    principal_type                         = optional(string, null)
   }))
   default = {
 
